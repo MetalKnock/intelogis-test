b9c0ae125500b96b8161ebbc2d2230eb427b8118
Dmitry Ivanov <dimannisik1@gmail.com>
2023-07-21 22:05:48 +0700

    fix: add display of marker icons

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -3,7 +3,6 @@ import { CustomPolyline } from '../CustomPolyline';
 import { MAP_ATTRIBUTION, MAP_CENTER, MAP_URL, MAP_ZOOM } from '@/constants/map';
 import { MarkerListContainer } from '../MarkerListContainer';
 import './Map.scss';
-import 'leaflet/dist/leaflet.css';
 
 const Map = () => {
   return (
